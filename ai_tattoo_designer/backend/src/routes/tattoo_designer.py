24815042aafecbb9a9ec28a5ffb62dd5f13090c8
lowlifescumm <86991950+lowlifescumm@users.noreply.github.com>
2025-06-18 05:38:15 -0700

    Update tattoo_designer.py

--- ai_tattoo_designer/backend/src/routes/tattoo_designer.py
+++ ai_tattoo_designer/backend/src/routes/tattoo_designer.py
@@ -295,7 +295,7 @@ def generate_image_with_replicate(image_prompt, first_name, last_name):
     model_version = "windxtech/tattoo-generator:0fe0fd450695b2fd99305d27a07ee6349943c200dc849d07633a98c24daef9a8" # This line was causing the error
     
     payload = {
-        "version": model_version,  # Remove  from here
+        "version": model_version,# Remove  from here
         "input": {
             "prompt": f"{image_prompt} - black and white tattoo design, detailed line art, mystical style",
             "width": 512,
